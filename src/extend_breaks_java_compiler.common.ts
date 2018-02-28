0e03f63e1154a9fd618b642cab4ee80705f1bad2
Adelrisk <adelrisk@users.noreply.github.com>
2018-02-28 14:44:25 +0100

    Add depdendency.

--- src/extend_breaks_java_compiler.common.ts
+++ src/extend_breaks_java_compiler.common.ts
@@ -2,12 +2,20 @@ import { Observable } from 'tns-core-modules/data/observable';
 import * as app from 'tns-core-modules/application';
 import * as dialogs from 'tns-core-modules/ui/dialogs';
 
+import * as webViewInterfaceModule from 'nativescript-webview-interface';
+
 export class Common extends Observable {
   public message: string;
 
+  public oWebViewInterface: webViewInterfaceModule.WebViewInterface;
   constructor() {
     super();
     this.message = Utils.SUCCESS_MSG();
+
+    /**
+     * This code ensures that the tree-shaking of the dependencies doesn't optimize away the critical code.
+     */
+    this.oWebViewInterface = new webViewInterfaceModule.WebViewInterface(null, '~/www/index.html');
   }
 
   public greet() {
